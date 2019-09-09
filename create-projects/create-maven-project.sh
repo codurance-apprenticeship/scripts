d2bf567211a7e5e0da049e938e5be1c9cf282458
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2019-09-09 16:41:10 +0100

    :sparkles: Maven project creation script - I have not tested that a lot :)

---
+++ create-projects/create-maven-project.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+PROJECT_NAME=$1
+if [ -z "$PROJECT_NAME" ]; then
+    echo "Please specify a project name"
+    exit 1
+fi
+
+echo Creating Maven project in $PWD/$PROJECT_NAME...
+mkdir -p $PROJECT_NAME/src/{main/com/$USER,test}/{java/com/$USER,resources}
+
+echo "<project>
+    <modelVersion>4.0.0</modelVersion>
+
+    <groupId>com.$USER</groupId>
+    <artifactId>$PROJECT_NAME</artifactId>
+    <version>1.0-SNAPSHOT</version>
+
+    <properties>
+        <java.version>1.8</java.version>
+    </properties>
+
+    <dependencies>
+        <dependency>
+            <groupId>org.junit.jupiter</groupId>
+            <artifactId>junit-jupiter-api</artifactId>
+            <version>5.5.1</version>
+            <scope>test</scope>
+        </dependency>
+
+        <dependency>
+            <groupId>org.junit.jupiter</groupId>
+            <artifactId>junit-jupiter-params</artifactId>
+            <version>5.5.1</version>
+            <scope>test</scope>
+        </dependency>
+
+        <dependency>
+            <groupId>org.mockito</groupId>
+            <artifactId>mockito-all</artifactId>
+            <version>1.10.19</version>
+            <scope>test</scope>
+        </dependency>
+    </dependencies>
+</project>
+" > $PROJECT_NAME/pom.xml
+
+echo ".idea
+*.iml
+*.ipr
+*.iws
+target" > $PROJECT_NAME/.gitignore
+
+cd $PROJECT_NAME
+git init
+git add -- pom.xml .gitignore
+git commit -m "Added build script and .gitignore"
+
+echo "All done. You can now import the project into your IDE."
